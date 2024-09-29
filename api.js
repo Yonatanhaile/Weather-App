76cecb8f545e650d1d381c76ec01f4c41864bf2b
Yonatan Haile <yonatanhaile06@gmail.com>
2024-09-29 12:21:20 +0300

    Edited the listen port

--- api.js
+++ api.js
@@ -216,6 +216,7 @@ app.post("/", function(req, res){
     });
 });
 
-app.listen(3000, function(){
-    console.log("Server is on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, function(){
+    console.log(`Server is running on port ${port}`);
 });
